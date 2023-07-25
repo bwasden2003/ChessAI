f9396ad3613b008fa29295cb7e0423f79117ef5e
bwasden2003 <bwasden2003@gmail.com>
2023-07-24 20:33:45 -0500

    started ai class

---
+++ src/ai.py
@@ -0,0 +1,34 @@
+
+from board import Board
+from game import Game
+
+
+class AI:
+
+	def __init__(self, game):
+		self.game = game
+	
+	def iterative_deepening(board, depth_limit):
+		best_move = None
+		pass
+
+	def alpha_beta_search(board, depth_limit):
+		best_value = float('-inf')
+		alpha, beta = float('-inf'), float('inf')
+		best_move = None
+		# loop through legal moves
+		
+
+	def evaluate(self):
+		# incorporate way to get piece scores for white and black
+
+		# calculate control score
+
+		# calculate safety score
+
+		# calculate pawn structure score
+
+		# need to determine weights for each factor
+
+		# return scores * weights
+		pass
